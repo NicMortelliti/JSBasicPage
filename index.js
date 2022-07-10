515edc8162ffdbddcd94331065b9c2db351d65fb
NicMortelliti <npmortelliti@gmail.com>
2022-07-10 16:40:40 -0700

    Create new DOM text elements on every button press

--- index.js
+++ index.js
@@ -14,8 +14,21 @@ function handleClickCounter() {
     // Update string
     counterLabel.innerText = `Clicked ${counterNumber} times`;
 
-    setTimeout(() => (counterLabel.innerText = ""), 3000);
+    // Append a new text line
+    createElements(counterNumber);
+
+    setTimeout(() => (counterLabel.innerText = ""), 1000);
   });
+}
+
+function createElements(number) {
+  let existingElement = document.getElementById("hr1");
+  let container = document.createElement("div");
+  let p = document.createElement("p");
+  container.appendChild(p);
+  existingElement.insertAdjacentElement("afterEnd", container);
+
+  p.after(`This is text #${number}`);
 }
 
 handleClickCounter();
